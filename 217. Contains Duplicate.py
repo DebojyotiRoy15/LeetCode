f22967b735b6d5fbd14af09c199762998d114a5f
Debojyoti Roy <57871787+DebojyotiRoy15@users.noreply.github.com>
2020-06-19 14:42:58 +0530

    Create 217. Contains Duplicate.py

---
+++ 217. Contains Duplicate.py
@@ -0,0 +1,9 @@
+#https://leetcode.com/problems/contains-duplicate/
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        st = set(nums)
+        if len(st) < len(nums):
+            return True
+        else:
+            return False
